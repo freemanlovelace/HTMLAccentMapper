7f8f16c5d2fbb13c8e53cc9ff8675cdf92b043ff
Freeman Lovelace <florentarnaza@gmail.com>
2024-02-03 10:33:23 -0500

    deleted unused file

--- setup.py
+++
@@ -1,8 +0,0 @@
-from cx_Freeze import setup, Executable
-
-setup(
-    name="HTMLAccentMapperExampleGui",
-    version="1.0",
-    description="map html accent entities to keyboard",
-    executables=[Executable("gui.py", base="tkinter")],
-)
